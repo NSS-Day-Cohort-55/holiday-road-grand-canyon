2d2c9aa84eddb7f03eb82f30ef37c19296c89a32
JavontaeH <hardeman.javontae@gmail.com>
2022-03-18 14:40:16 -0500

    fixed wrong key

--- src/scripts/weather/WeatherDataManager.js
+++ src/scripts/weather/WeatherDataManager.js
@@ -1,23 +1,28 @@
 import settings from "settings.js";
 
+lat = 0;
+lon = 0;
+
 export const getGeocode = (city, state, countryCode) => {
   return fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&limit=1&appid=${settings.npsKey}`
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${countryCode}&limit=1&appid=${settings.weatherKey}`
   )
     .then((response) => response.json())
     .then((json) => {
       // do something
+      console.log(json);
       return json;
     });
 };
 
 export const getWeatherReport = (lat, lon) => {
   return fetch(
-    `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${settings.npsKey}`
+    `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${settings.weatherKey}`
   )
     .then((response) => response.json())
     .then((json) => {
       // do something
+      console.log(json);
       return json;
     });
 };
