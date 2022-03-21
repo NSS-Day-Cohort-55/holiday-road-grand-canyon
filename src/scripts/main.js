a7c8371b62d179653603c6fde72593b8cd02eedb
rushayhays <rushayhays@outlook.com>
2022-03-21 10:13:22 -0500

    State dropdown list created, some work on eateriy drop down list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 import * as WeatherDataManager from "./weather/WeatherDataManager.js";
+import { getStates } from "./directions/DirectionDataManager.js"
+import { stateSelectionFormatter } from "./directions/states.js"
+import { getEateries } from "./eateries/EateryDataManager.js"
 
 // temp code for getting weather report
 WeatherDataManager.getGeocode("Nashville", "TN", "USA").then(
@@ -9,3 +12,12 @@ WeatherDataManager.getGeocode("Nashville", "TN", "USA").then(
     );
   }
 );
+
+//some code for getting all the states for the state drop down box
+getStates().then(allStates => {
+  stateSelectionFormatter(allStates)
+} )
+
+//some code for getting all the eateries for the eatery drop down box
+
+getEateries();
