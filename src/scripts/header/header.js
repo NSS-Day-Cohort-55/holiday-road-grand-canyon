0541bf74a0d00177856382961bd1645281d6d3a1
JavontaeH <hardeman.javontae@gmail.com>
2022-03-23 10:47:27 -0500

    styled header and moved aside to header

--- src/scripts/header/header.js
+++ src/scripts/header/header.js
@@ -1,10 +1,13 @@
 export const startHeader = () => {
   document.querySelector(
     ".header"
-  ).innerHTML = `<div class="header_logo">Grand canyon Travel Planners</div>
+  ).innerHTML = `<div class="header_logo">Grand Canyon Travel Planners</div>
+  <div class="welcome_aside">
         <div class="header_welcomeMessage">Welcome So-and-So</div>
+        <button id="openbtn" class="openbtn"> &#9776; Saved Trips</button>
+        </div>
         <div class="header_startingPointMessage">
-            Your Starting Point
+            Your Starting Point...
             <div class="header_startingPoint">Nashville,TN</div>
         </div>`;
 };
